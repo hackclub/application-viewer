de358ee9962d198e1c08dbfb16a6e09ccd5573a9
leomcelroy <leomcelroy@gmail.com>
2021-08-31 16:50:56 -0400

    Update newTrackedApp.js

--- pages/api/newTrackedApp.js
+++ pages/api/newTrackedApp.js
@@ -7,19 +7,38 @@ export default async function handler(req, res) {
 
   try {
     const application = (await base("Application Database").find(req.query.app)).fields;
+
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+
+    const newdate = year + "/" + month + "/" + day;
     const newTrackedApp = {
       "Venue": application["School Name"],
-      "Address": application["School Address"],
-      "Leaders": application["Full Name"],
-      "Leaders' Emails": application["Leaders Emails"],
-      "Applied": Date.now(),
+      "Location": application["School Address"],
+      "Leaders": application["Full Name"].join(","),
+      "Leaders' Emails": application["Leaders Emails"].join(","),
+      "Applied": newdate,
       "Status": "applied",
       "Application": `https://application-viewer.hackclub.dev/?app=${req.query.app}`,
       "App ID": req.query.app,
-      "Location": application["School Address"], // should I do google maps pin?
       // newTrackedApp.duplicate = application["Prospective Leaders"], // need to check current clubs
       // newTrackedApp.ambassador = application["Prospective Leaders"], // india or not
     };
+
+    const postCreate = (err, records) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // records.forEach(function (record) {
+      //   console.log(record.getId());
+      // });
+    }
+
+    const tracked = await base("Application Tracker");
+    tracked.create(newTrackedApp, postCreate);
 
     res.status(200).json(newTrackedApp);
   } catch (e) {
