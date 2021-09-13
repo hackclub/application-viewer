91059dd9f363c20f0968980a1c2b035909dba275
Max Wofford <max@maxwofford.com>
2021-09-13 17:09:43 -0400

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+const NotFound = () => (
+  <>
+    <h1>404, club not found</h1>
+    <img src="https://cloud-ppapjvtsa-hack-club-bot.vercel.app/0club_dinosaur.png" />
+  </>
+)
+
+export default NotFound
