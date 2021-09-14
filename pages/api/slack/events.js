d1f00bd5e7b89a2d2fdf61fb1a6d2687d51e2ae6
Max Wofford <max@maxwofford.com>
2021-09-14 16:48:20 -0400

    Cleanup unused lines in slack/events

--- pages/api/slack/events.js
+++ pages/api/slack/events.js
@@ -22,8 +22,6 @@ export default async (req, res) => {
   ensureMethod({ req, method: 'POST' })
 
   try {
-
-
     console.log(`Got a '${req.body.type}' event from Slack...`)
 
     switch (req.body.type) {
@@ -43,21 +41,12 @@ export default async (req, res) => {
         const mentionSubstring = '<@ULG7GRP0A>' // @bouncer
         if (bouncer_channels.includes(event.channel) && event.type == 'message' && event.text.startsWith(mentionSubstring)) {
           console.log('...responding!')
-          const { user, text, ts, channel } = event
+          const { user, text, channel } = event
           const cleanedText = text.replace(mentionSubstring, '').trim()
           const club = await airtable.find('Application Tracker', `{Check-In Pass}='${cleanedText}'`)
           if (club) {
-            // console.log(`Adding LEADER '${user}' to CLUB'${club.id}'`)
-            // console.log('Old value is:', club.fields['Leader Slack IDs'])
-            // const leaders = club.fields['Leader Slack IDs'] || ''
-            // const leaderIDsSet = new Set(leaders.split(','))
-            // leaderIDsSet.add(user)
-            // await airtable.patch(Array.from(leaderIDsSet).join(','))
-            // console.log('New value is:', Array.from(leaderIDsSet).join(','))
-
-            await slackPostMessage({ channel, text: `<@${user}>, run along and join your team in <#${club.fields['Slack Channel ID']}>.` })
+            await slackPostMessage({ channel, text: `you got it <@${user}>, run along and join your team in <#${club.fields['Slack Channel ID']}>` })
           } else {
-            // pity the foo'
             await slackPostMessage({ channel, text: `what kinda crazy mumbo-jumbo nonsense is this?? I could find a solid _nobody_ in our applications database with the registration passphrase "${cleanedText}". try again, fool.` })
           }
         } else {
