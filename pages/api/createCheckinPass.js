fea158e9e4c78e68b98ca95cf2f32f01f8fcd7d3
Max Wofford <max@maxwofford.com>
2021-09-14 14:00:30 -0400

    Save check-in passwords to airtable

--- pages/api/createCheckinPass.js
+++ pages/api/createCheckinPass.js
@@ -1,11 +1,16 @@
 import ensureMethod from "../../utils/ensureMethod";
+import airtable from "../../utils/airtable";
 
 export default async (req, res) => {
   const { recordID } = req.body
 
   try {
     ensureMethod({req, method: 'POST'})
-    res.send({password: 'hunter2!'})
+    const password = Math.random().toString(16).slice(2)
+    await airtable.patch('Application Tracker', recordID, {
+      'Check-In Pass': password
+    })
+    res.send({password})
   } catch (err) {
     console.error(err);
     res.status(err.status || 500).send(err);
