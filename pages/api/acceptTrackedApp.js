43e3a267fc119b47e404cbbc07fa0314383d52a3
Max Wofford <max@maxwofford.com>
2021-09-15 17:25:59 -0400

    Add slack post on accept/reject

--- pages/api/acceptTrackedApp.js
+++ pages/api/acceptTrackedApp.js
@@ -3,6 +3,8 @@ import ensureMethod from "../../utils/ensureMethod";
 import createSlackChannel from "../../utils/createSlackChannel";
 import sendEmail from "../../utils/sendEmail";
 import slackReact from "../../utils/slackReact";
+import slackPostMessage from "../../utils/slackPostMessage";
+import transcript from "../../utils/transcript";
 
 export default async (req, res) => {
   const { recordID, email } = req.body
@@ -23,22 +25,24 @@ export default async (req, res) => {
     email.content = email.content
       .replace('%SLACK_URL%', `https://app.slack.com/client/T0266FRGM/${channelID}`)
 
-    // update record
-    await airtable.patch('Application Tracker', recordID, {
+    const promises = []
+    promises.push(airtable.patch('Application Tracker', recordID, {
       "Notes": note,
       "Status": "awaiting onboarding",
       "Slack Channel ID": channelID,
       "Date Responded": new Date().toISOString().slice(0, 10)
-    })
+    }))
 
-    // send email
-    await sendEmail(email);
+    promises.push(sendEmail(email));
 
     // update slack thread
     const channel = 'C02F9GD407J' /* #application-conspiracy */
     const timestamp = trackedApp.fields["Application Committee Timestamp"];
-    console.log(await slackReact({channel, timestamp, name: 'white_check_mark'}))
-    console.log(await slackReact({channel, timestamp, name: 'no_entry', addOrRemove: 'remove'}))
+    promises.push(slackReact({channel, timestamp, name: 'white_check_mark'}))
+    promises.push(slackReact({channel, timestamp, name: 'no_entry', addOrRemove: 'remove'}))
+    promises.push(slackPostMessage({channel, timestamp, text: transcript('application-committee.accepted', {channel: channelID})}))
+
+    await Promise.all(promises)
 
     res.send({ ok: true })
 
--- pages/api/rejectTrackedApp.js
+++ pages/api/rejectTrackedApp.js
@@ -2,6 +2,8 @@ import airtable from "../../utils/airtable";
 import ensureMethod from "../../utils/ensureMethod";
 import sendEmail from "../../utils/sendEmail";
 import slackReact from "../../utils/slackReact";
+import slackPostMessage from "../../utils/slackPostMessage";
+import transcript from "../../utils/transcript";
 
 export default async (req, res) => {
   const { recordID, teacher, email } = req.body
@@ -17,20 +19,23 @@ export default async (req, res) => {
       ? `${currentEntryNote}\nUpdated with webhook: ${teacher ? "teacher" : "reject"}`
       : `Updated with webhook: ${teacher ? "teacher" : "reject"}`
 
-    await airtable.patch('Application Tracker', recordID, {
+    const promises = []
+    promises.push(airtable.patch('Application Tracker', recordID, {
       "Notes": note,
       "Status": "rejected",
       "Date Responded": new Date().toISOString().slice(0, 10)
-    })
+    }))
 
     // send email
-    await sendEmail(email);
+    promises.push(sendEmail(email));
 
-    // update slack thread
     const channel = 'C02F9GD407J' /* #application-conspiracy */
     const timestamp = trackedApp.fields["Application Committee Timestamp"];
-    console.log(await slackReact({channel, timestamp, name: 'no_entry'}))
-    console.log(await slackReact({channel, timestamp, name: 'white_check_mark', addOrRemove: 'remove'}))
+    promises.push(slackReact({channel, timestamp, name: 'no_entry'}))
+    promises.push(slackReact({channel, timestamp, name: 'white_check_mark', addOrRemove: 'remove'}))
+    promises.push(slackPostMessage({channel, timestamp, text: transcript('application-committee.rejected')}))
+
+    await Promise.all(promises)
 
     res.send({ ok: true })
 
