0255ff4695d86f3d95573419693ebfeb50de9a23
Max Wofford <max@maxwofford.com>
2021-09-15 17:31:23 -0400

    Add support for thread replies

--- utils/slackPostMessage.js
+++ utils/slackPostMessage.js
@@ -1,15 +1,19 @@
-export default async ({ channel, text }) => {
-  console.log(`Posting in CHANNEL '${channel}' TEXT '${text}'`)
+export default async ({ channel, text, timestamp }) => {
+  console.log(`${timestamp ? 'Replying' : 'Posting'} in CHANNEL '${channel}' TEXT '${text}'`)
+
+  const body = { channel, text }
+
+  if (timestamp) {
+    body.reply_ts = timestamp
+  }
+
   const result = await fetch('https://slack.com/api/chat.postMessage', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.BOUNCER_SLACK_TOKEN}`
     },
-    body: JSON.stringify({
-      channel,
-      text,
-    })
+    body: JSON.stringify(body)
   }).then(r => r.json()).catch(err => {
     console.error(err)
   });
