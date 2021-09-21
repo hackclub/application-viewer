a955e8d34c06b9114e017a864a3fb1c0b9a326e2
Max Wofford <max@maxwofford.com>
2021-09-20 22:55:11 -0400

    AWS -> SES b/c vercel reserved envs

--- utils/email.js
+++ utils/email.js
@@ -2,9 +2,9 @@ import AWS from 'aws-sdk'
 import {Converter} from 'showdown'
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.SES_ACCESS_KEY_ID,
+  secretAccessKey: process.env.SES_SECRET_ACCESS_KEY,
+  region: process.env.SES_REGION,
 })
 
 const ses = new AWS.SES({ apiVersion: '2010-12-01' })
