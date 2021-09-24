c5feb2ea60978a67af35645beb59cea9b26f4422
bajpai244 <bajpaiharsh244@gmail.com>
2021-09-24 19:37:18 +0530

    add test page

---
+++ pages/test.js
@@ -0,0 +1,32 @@
+/** @format */
+
+import { Button } from '@theme-ui/components';
+import { ThemeProvider } from '@theme-ui/theme-provider';
+import { getSession, signOut } from 'next-auth/client';
+import { signIn } from 'next-auth/dist/client';
+import Auth from '../js/components/Auth';
+
+import theme from '../js/theme/index';
+
+const Test = ({ ses }) => {
+  console.log('session is', ses);
+
+  try {
+  } catch (err) {}
+
+  if (!ses) return <Auth />;
+
+  return (
+    <div>
+      yay <Button onClick={signOut}>Sign Out</Button>{' '}
+    </div>
+  );
+};
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+
+  return { props: { ses: session } };
+};
+
+export default Test;
