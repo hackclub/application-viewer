fbd618af34106b5ae90f367f79274ed29984544d
leomcelroy <leomcelroy@gmail.com>
2021-09-27 13:39:50 -0400

    Update accept.js

--- js/emails/accept.js
+++ js/emails/accept.js
@@ -6,17 +6,17 @@ Welcome to Hack Club!
 We loved your application and think you’ll run a great club at your school. 
 There are just two more steps to complete before you’re officially *in*: 
 
-1. [Join us on Slack!](https://hackclub.com/slack/). 
+1. [Join us on Slack!](https://hackclub.com/slack/) 
 This is our community’s main communication platform and where you’ll get to
 meet other club leaders and stay up-to-date on everything going on at Hack Club. 
 
 2. Schedule a 30-minute onboarding call with us. 
 These calls are a way to get to know each other and familiarize you with some 
-tools to help you run a great club. 
+tools to help run a great club. 
 Please respond to this email with three weekday times that 
 work for you in the next week or so and we’ll get that scheduled right away. 
 
-Can’t wait to meet you, chat soon!
+Can’t wait to meet, talk soon!
 
 Best,
 
