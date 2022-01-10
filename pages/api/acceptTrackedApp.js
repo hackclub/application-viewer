94571d2dcd3e81489d6fde9d6c1892913e0ac0d1
leomcelroy <leomcelroy@gmail.com>
2022-01-10 09:27:53 -0500

    Update acceptTrackedApp.js

--- pages/api/acceptTrackedApp.js
+++ pages/api/acceptTrackedApp.js
@@ -1,6 +1,5 @@
 import airtable from "../../utils/airtable";
 import ensureMethod from "../../utils/ensureMethod";
-import createSlackChannel from "../../utils/createSlackChannel";
 import slackReact from "../../utils/slackReact";
 import slackPostMessage from "../../utils/slackPostMessage";
 import transcript from "../../utils/transcript";
@@ -19,8 +18,6 @@ export default async (req, res) => {
       return
     }
 
-    const channelID = await createSlackChannel(recordID);
-
     const trackedApp = await airtable.find('Application Tracker', recordID)
 
     const currentEntryNote = trackedApp.fields["Notes"];
@@ -29,14 +26,26 @@ export default async (req, res) => {
       ? `${currentEntryNote}\nUpdated with webhook: accept`
       : `Updated with webhook: accept`
 
-    email.message = email.message
-      .replace('%SLACK_URL%', `https://app.slack.com/client/T0266FRGM/${channelID}`)
+    const possibleAmbassadors = {
+      "hq": "HQ",
+      "brasil": "Brasil",
+      "apac": "APAC"
+    }
+
+    const emailFromExtension = email.from.split("+")[1];
+    const ambassador = (emailFromExtension in possibleAmbassadors) 
+      ? possibleAmbassadors[emailFromExtension] 
+      : "";
+
+    // email.message = email.message
+    //   .replace('%SLACK_URL%', `https://app.slack.com/client/T0266FRGM/${channelID}`)
 
     const promises = []
     promises.push(airtable.patch('Application Tracker', recordID, {
       "Notes": note,
       "Status": "awaiting onboarding",
-      "Slack Channel ID": channelID,
+      "Ambassador": ambassador,
+      // "Slack Channel ID": channelID,
       "Date Responded": new Date().toISOString().slice(0, 10)
     }))
 
