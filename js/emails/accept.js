c0f08be4c2b473014b4a7eda7358650d1f89ea4c
leomcelroy <leomcelroy@gmail.com>
2022-01-10 09:18:42 -0500

    Update accept.js

--- js/emails/accept.js
+++ js/emails/accept.js
@@ -3,7 +3,6 @@ export const accept = (names) => `Hey ${names},
 
 Welcome to Hack Club! 
 
-We loved your application and think you’ll run a great club at your school. 
 There are just two more steps to complete before you’re officially *in*: 
 
 1. [Join us on Slack!](https://hackclub.com/slack/) 
@@ -14,7 +13,7 @@ meet other club leaders and stay up-to-date on everything going on at Hack Club.
 These calls are a way to get to know each other and familiarize you with some 
 tools to help run a great club. 
 Please respond to this email with three weekday times that 
-work for you in the next week or so and we’ll get that scheduled right away. 
+work for you in the next week or so (write your time zone too) and we’ll get that scheduled right away. 
 
 Can’t wait to meet, talk soon!
 
