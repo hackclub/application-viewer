7f21536675dd743a17f97c91fc4bf16fd7617b46
PellMellKid <109363156+PellMellKid@users.noreply.github.com>
2022-07-15 12:57:42 -0400

    Update reject.js

--- js/emails/reject.js
+++ js/emails/reject.js
@@ -2,7 +2,8 @@ export const reject = () => `
 Hello,
 
 Thanks for applying to start a Hack Club. (Because of these reasons) we've decided not to move forward with your club.
-It is important to note that we reject applications, not people. We'd recommend you (do these things) then we encourage you to apply again!
+It is important to note that we reject applications, not people. We'd highly recommend that you take active steps toward setting up your club like 
+collecting signup or designing activities then we encourage you to apply again!
 
 We’d love to answer any questions you have, just reply to this email.
 
