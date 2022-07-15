db1f2ea05a5f733e0df3f6c889ddc3eb1e634aaf
PellMellKid <109363156+PellMellKid@users.noreply.github.com>
2022-07-15 11:55:23 -0400

    Update accept.js

--- js/emails/accept.js
+++ js/emails/accept.js
@@ -7,16 +7,15 @@ There’s only 2 things left to do:
 
 1. [Join us on Slack!](https://hackclub.com/slack/) 
 If you’re not already on our Slack, you should _definitely_ be on our Slack. 
-This is one of the best parts of being part of Hack Club! It’s **_the best_**place to:
-* Meet other teenagers like you!
-* Get help if you’re stuck!
-* Get invites to events!
-* Get inspired! 
-* Get involved in cool projects!
-* Stay up-to-date on everything going on at Hack Club!
-
-2. Schedule a 30-minute welcome call with me! 
-We’ll meet to make sure you have everything you need to run an amazing club!
+This is one of the best parts of being part of Hack Club! It’s **_the best_** place to:
+    * Meet other teenagers like you!
+    * Get help if you’re stuck!
+    * Get invites to events!
+    * Get inspired! 
+    * Get involved in cool projects!
+    * Stay up-to-date on everything going on at Hack Club!
+2.Schedule a 30-minute Welcome Call with me! 
+We’ll meet over Zoom to make sure you have everything you need to run an amazing club!
 
 **Please respond to this email with three weekday times that work for you in the next week or 
 so (include your time zone!) and I’ll be happy to schedule a meeting together.**
