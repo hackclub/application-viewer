63341ec90d702963ee119937f15428805b91a8a6
Cedric Hutchings <ced@misguided.enterprises>
2022-07-15 11:54:38 -0400

    Update reject.js

--- js/emails/reject.js
+++ js/emails/reject.js
@@ -7,7 +7,7 @@ It is important to note that we reject applications, not people.
 You are always welcome to submit another application. 
 We highly recommend taking active steps toward setting up your club beforehand, such as collecting signups, running marketing campaigns, designing activities, or holding meetings.
 
-If we can answer any questions about your application, feel free to reply to this email and we'll get back to you.
+If we can answer any q about your application, feel free to reply to this email and we'll get back to you.
 
 Thank you for your interest!
 
