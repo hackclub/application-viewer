fe411453ce77b2795db95a9bd508f9514a028068
PellMellKid <109363156+PellMellKid@users.noreply.github.com>
2022-07-22 09:09:21 -0400

    Update accept.js

--- js/emails/accept.js
+++ js/emails/accept.js
@@ -1,29 +1,33 @@
 
-export const accept = (names) => `Hey ${names},
+export const accept = (names) => `Hi ${names},
 
-Welcome to Hack Club! We loved your application and we're so excited to welcome you into a community of teens like you. 
-As a Hack Club Leader, you'll inspire others to make things they're proud. You'll learn a lot and we'll be here for you every step of the way!
+First, I just want to say thank you - I really enjoyed reading the application you wrote about your Hack Club, and I’m happy to share that we’re moving forward! Congratulations!
 
-There’s only 2 things left to do:
+To introduce myself, I’m Holly from Hack Club HQ in Vermont, and I’m going to be your point of contact at HQ. My job is to be here for you, where you can reach out to me for anything including over text or call (my number is 802-238-7261).
 
-1. [Join us on Slack!](https://hackclub.com/slack/) 
-If you’re not already on our Slack, you should _definitely_ be on our Slack. 
-This is one of the best parts of being part of Hack Club! It’s **_the best_** place to:
-    * Meet other teenagers like you
-    * Get help if you’re stuck
-    * Get invites to events
-    * Get inspired 
-    * Get involved in cool projects
-    * Stay up-to-date on everything going on at Hack Club
-    
-2. Schedule a 30-minute Welcome Call with us! 
-We’ll meet over Zoom to make sure you have everything you need to run an amazing club.
+![Holly Waving](https://mail.google.com/mail/u/0?ui=2&ik=839e921c5d&attid=0.1&permmsgid=msg-a:r6262172848995221536&th=182226f40497fca7&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ8J_1u27RH8geIqCpM7Sk_mWfV6ep_IMfuSgcYkleiOsujRCLOcThh6jKPJZAajxNBiY3jgXhl82m81zhpnBfdiMPbfyDjYEX_F-3z-nGGMvr2b3gkuAHWUQCM&disp=emb&realattid=ii_l5vanevx0)
 
-**Please respond to this email with three weekday times that work for you in the next week or 
-so (include your time zone!) and I’ll be happy to schedule a meeting together.**
+You can come to me for everything from marketing advice to thinking about starting a hackathon (like [Assemble](https://assemble.hackclub.com/), which was run by [@sampoder](https://hackclub.slack.com/team/USNPNJXNX), 17, from Singapore). My goal is to be your best friend inside of Hack Club, and to do everything I can to support you and your team.
 
-Can’t wait to meet, talk soon!
+As a next step, I’d like to set up a call for you and me to meet over Zoom. In this call, we'll get to know each other, I'll get you set up with all of the different Hack Club resources, and we'll talk through the next steps for your club.
+
+**Can you send me 3 times over the next week that work for a 45 minute Zoom call?** I’m online Monday - Friday, and am typically available until 8 PM Eastern / 5 PM Pacific on Mondays and Tuesdays, and until 5 PM Eastern / 2 PM Pacific Wednesdays - Fridays.
+
+One thing that’s important to note is that on the call, we’ll focus on how Hack Club can support you without telling you how to run your club. You know your high school better than anyone else in the world, and you’re going to know what’s going to work best. What would have worked great in [the high school I went to](https://www.google.com/maps/place/Hodgdon+High+School/@46.0542533,-67.86672,17z/data=!3m1!4b1!4m5!3m4!1s0x4ca4c2126935387f:0x77e39eaad04f0993!8m2!3d46.0542539!4d-67.864532) probably wouldn’t work at your school - and vice versa!
+
+I'm so looking forward to getting to know you, and doing my best to support you as you embark on this journey.
+
+Cheers!
+Holly
+
+(p.s. I just put a package with stickers in the mail to both of you, should arrive in 2-3 days! DM me on Slack when you get it!)
 
 Best,
 
-Holly @ Hack Club HQ`
+Holly Delisle
+Club Operations Lead
+
+![Flag](https://assets.hackclub.com/flag-standalone.svg)
+Hack Club is an [open source](https://github.com/hackclub/hackclub) and [financially transparent](https://bank.hackclub.com/hq/) 501(c)(3) nonprofit. Our EIN is 81-2908499. By the students, for the students.
+
+[Clubs](https://hackclub.com/) • [Slack](https://hackclub.com/slack/) • [Bank](https://hackclub.com/bank/) • [Hackathons](https://hackathons.hackclub.com/)`
