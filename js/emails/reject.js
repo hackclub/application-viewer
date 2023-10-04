d13664f0a360f354c75bb59696a013516f001a04
Holly Delisle <holly@hackclub.com>
2023-10-04 11:19:36 -0400

    Update reject.js
    change wording: college/universities

--- js/emails/reject.js
+++ js/emails/reject.js
@@ -1,8 +1,8 @@
 export const reject = () => `
 Hello,
 
-Thanks for applying to start a Hack Club. After review, we've decided not to move forward with your club right now.
-We deny applications, not people. We'd highly recommend that you take steps toward setting up your club like collecting signups or designing activities then we encourage you to apply again!
+Thanks for applying to start a Hack Club. As much as I enjoyed reading your application, I'm sorry to say that we are not able to move forward with your club.
+We no longer accept applications from college or university students so that we can focus on our mission of bringing high school students together to make interesting things with code in high school clubs.
 
 We’d love to answer any questions you have, just reply to this email.
 
