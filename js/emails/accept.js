45b502282d9e3356349f2d6e9a29923a9cc1c6a5
Arpan Pandey <arpan@hackclub.com>
2024-06-25 08:00:14 +0530

    Update accept.js

--- js/emails/accept.js
+++ js/emails/accept.js
@@ -107,7 +107,39 @@ Hack Club is an [open source](https://github.com/hackclub/hackclub) and [financi
 [Clubs](https://hackclub.com/) • [Slack](https://hackclub.com/slack/) • [HCB](https://hackclub.com/hcb/) • [Hackathons](https://hackathons.hackclub.com/)`
 )
     } else if(from == "arpan@hackclub.com") {
-        return(`Arpan email template`)
+        return(`Hey ${names},
+
+I just want to say that I really enjoyed reading your application, and I’m happy to share that we’re moving forward! Congratulations!
+
+I’m Arpan from Hack Club HQ (currently in India) in Vermont, and I’ll be your point of contact at HQ. Come to me for anything from marketing advice to thinking about starting a hackathon (like [Assemble](https://assemble.hackclub.com/), which was run by [@sampoder](https://hackclub.slack.com/team/USNPNJXNX), 17, from Singapore). You can even text or call me - my number is +91 8826810708.
+
+Let's set up a call for us to meet over Zoom. We'll get to know each other, I'll set you up with all the different Hack Club resources, and we'll talk through the next steps for your club.
+
+<img src="https://cloud-iibplai1r-hack-club-bot.vercel.app/0img_20221231_081104.jpg" alt="Arpan and Thomas @ Epoch" width="240px" />
+
+**Can you send me 3 times over the next week that work for a 45-minute Zoom call?** I’m online Tuesday - Thursday, and available from 4PM IST to 9PM IST.
+
+I'm so looking forward to getting to know you and supporting you as you embark on this journey.
+
+Cheers!
+Arpan
+
+(p.s. I just put a package with stickers in the mail to you! DM me on Slack when you get it!)
+
+--
+
+**Arpan Pandey**
+
+Club Operations and Engineering, Hack Club
+Hack Club Slack: [@A](https://hackclub.slack.com/team/U0409FSKU82) (join the Slack [here](https://hackclub.com/slack/))
+Call/Text: +91 8826810708
+Email: [arpan@hackclub.com](arpan@hackclub.com)
+
+<img src="https://assets.hackclub.com/flag-standalone.png" alt="Hack Club Flag" width="100px" />
+
+Hack Club is an [open source](https://github.com/hackclub/hackclub) and [financially transparent](https://bank.hackclub.com/hq/) 501(c)(3) nonprofit. Our EIN is 81-2908499. By the students, for the students.
+
+[Clubs](https://hackclub.com/) • [Slack](https://hackclub.com/slack/) • [HCB](https://hackclub.com/hcb/) • [Hackathons](https://hackathons.hackclub.com/)`)
     } else if(from == "sarah@hackclub.com") {
         return(`
 Hi ${names},
