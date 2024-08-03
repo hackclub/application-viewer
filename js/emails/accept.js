473930940dc27eded443971d3e5ccf87a403b290
Sahiti Dasari <sahitiaws@gmail.com>
2024-08-02 22:37:16 -0400

    Update accept.js

--- js/emails/accept.js
+++ js/emails/accept.js
@@ -47,7 +47,7 @@ I’m Sahiti from Hack Club HQ (currently in Georgia), and I’ll be your point 
 
 Let's set up a call for us to meet over Zoom. We'll get to know each other, I'll set you up with all of the different Hack Club resources, and we'll talk through the next steps for your club.
 
-**Can you send me 3 times over the next week that work for a 45 minute Zoom call?** I’m online on weekdays from 10 AM to 4 PM on most days.
+**Can you send me 3 times over the next week that work for a 45 minute Zoom call?** I’m online Wednesday and Friday after 3 PM EST and all day on weekends on most days.
 
 I'm so looking forward to getting to know you and supporting you as you embark on this journey.
 
@@ -72,40 +72,6 @@ Hack Club is an [open source](https://github.com/hackclub/hackclub) and [financi
 
             
             `)
-    } else if(from == "jasper@hackclub.com") {
-        return (
-        `Hi ${names},
-
-I just want to say that I LOVEED reading your application, and am happy to share that we’re moving forward! Congratulations!
-
-I’m Jasper from Hack Club HQ in Vermont, and I’ll be your point of contact at HQ. You can come to me for anything from marketing advice, to thinking about starting a hackathon (like [Assemble](https://assemble.hackclub.com/), which was run by [@sampoder](https://hackclub.slack.com/team/USNPNJXNX), 17, from Singapore), ideas for club meetings, or even if you just want some Hack Club Stickers! :) 
-
-<img src="https://cloud-i7yb9x3hw-hack-club-bot.vercel.app/0jasper-wth-the-dog.jpg" alt="Jasper Waving" width="240px" />
-
-I would love to set up a call for us to meet over Zoom! We'll get to know each other, I'll set you up with all of the different Hack Club resources, and we'll talk through the next steps for your club.
-
-**Can you please use [this link](https://hack.af/jm-club-onboarding) [(https://hack.af/jm-club-onboarding)](https://hack.af/jm-club-onboarding) to schedule some time with me over the next week or so that will work for a 20-30 minute Zoom call?** I’m online Monday - Friday, and typically available between 4pm Eastern Time (1 PM Pacific) and 9 PM Eastern Time (6 PM Pacific). Let me know if these times don’t work for you, and we can make a time that works for you!
-
-I'm so looking forward to getting to know you and supporting you and your new Hack Club!
-
-Jasper
-
-(p.s. I just put a package with stickers in the mail to you! DM me on Slack when you get it!)
-
---
-
-**Jasper Mayone**
-
-Clubs Team, Hack Club  
-Hack Club Slack:[@Jasper](https://hackclub.slack.com/team/U05NX48GL3T) (join the Slack [here](https://hackclub.com/slack/))  
-Email: [jasper@hackclub.com](jasper@hackclub.com)
-
-<img src="https://assets.hackclub.com/flag-standalone.png" alt="Hack Club Flag" width="100px" />
-
-Hack Club is an [open source](https://github.com/hackclub/hackclub) and [financially transparent](https://bank.hackclub.com/hq/) 501(c)(3) nonprofit. Our EIN is 81-2908499. By the students, for the students.
-
-[Clubs](https://hackclub.com/) • [Slack](https://hackclub.com/slack/) • [HCB](https://hackclub.com/hcb/) • [Hackathons](https://hackathons.hackclub.com/)`
-)
     } else if(from == "arpan@hackclub.com") {
         return(`Hey ${names},
 
